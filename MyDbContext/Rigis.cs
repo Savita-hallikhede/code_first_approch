1cc6d208f57bca73234e76c063c4fb80d54725a3
savit <savit@LAPTOP-J9H70FJT>
2026-01-30 06:07:37 +0530

    ADDED new classs for register

---
+++ MyDbContext/Rigis.cs
@@ -0,0 +1,16 @@
+﻿namespace code_first_approch.MyDbContext
+{
+    public class Rigis
+    {
+
+         
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Author { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string PhoneNumber { get; set; }
+            
+            
+    }
+}
