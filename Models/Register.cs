fe57cd62a503fe61a4c9bca8d7b1d5ec02a08078
savit <savit@LAPTOP-J9H70FJT>
2026-01-30 06:25:41 +0530

    created a class in a model folder for registration

---
+++ Models/Register.cs
@@ -0,0 +1,13 @@
+﻿namespace code_first_approch.Models
+{
+    public class Register
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Phone { get; set; }
+        public string PhoneNumber { get; set; }
+
+    }
+}
